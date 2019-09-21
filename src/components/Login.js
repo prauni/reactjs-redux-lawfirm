a956457af92e2990bafd40fc72d15c8c1c0b0b58
Riyan Bemachadan <riyan04314@gmail.com>
2019-09-21 08:03:52 +0530

    React :: Ajax :: Partially Done :: CROS issue pending

--- src/components/Login.js
+++ src/components/Login.js
@@ -30,14 +30,46 @@ export default class Login extends Component{
 		e.preventDefault()
 		const  { username, password } = this.state
 		
+		
+		const formData = new FormData();
+		formData.append('prodID',7);
+		const apiUrl = 'http://localhost/test.php';
+		const options = {
+			method:'GET',
+			body:formData,
+			mode: 'no-cors'
+		}
+
+		fetch(apiUrl,options)
+			.then(res => res.json())
+			.then(
+				(result) => {
+					console.log('Ajax fetch success.... ')
+				},
+				(error) =>{
+					console.log('Ajax fetch error.... ')
+				}				
+			)
+		/*
+		'http://localhost/test.php'
+		*/
+		$.ajax({
+			url:'http://jsonplaceholder.typicode.com/posts',
+			success:(data)=>{
+				console.log('Ajax success ');
+				//console.log(data)
+			},
+			error:(err)=>{
+				console.log('Ajax error ')
+			}
+		})
+
 		if(username === "X" && password === "Y"){
 			localStorage.setItem("token","helloworld")
 			this.setState({
 				loggedIn:true
 			})
-		}
-		
-		
+		}		
 		
 	}
 	
@@ -48,7 +80,7 @@ export default class Login extends Component{
 			
 		return (
 			<div>
-				<h1>Login</h1>
+				<h1>Login Page</h1>
 				<form onSubmit={this.submitForm}>
 					<input name="username" value={this.state.username} onChange={this.onChange} type="text" placeholder="Username" /> <br />
 					<input name="password" value={this.state.password} onChange={this.onChange} type="text" placeholder="Password" /> <br />
